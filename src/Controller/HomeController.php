070c333387d090d2e816e5ccac2c589c0ed46c2a
450Oucema <58775200+450Oucema@users.noreply.github.com>
2020-02-11 10:12:59 +0100

    2nd commit, découverte de doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,20 +1,20 @@
 <?php
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
-class HomeController{
-    /**
-     *@var Environment
-     */
-    private $twig;
+class HomeController extends AbstractController {
 
-    public function __construct( Environment $twig)
-    {
-        $this->twig = $twig;
-    }
+    /**
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     *
+     */
 
     public function index(): Response{
-        return new Response($this->twig->render('pages/home.html.twig'));
+        return $this->render('pages/home.html.twig',['current_menu' => 'home']);
     }
 }
