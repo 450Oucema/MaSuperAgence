18760343f69fbacd4cbe2e14d4dd7d24f1c57c35
450Oucema <58775200+450Oucema@users.noreply.github.com>
2020-02-11 17:19:44 +0100

    CRU Fonctionnel, 4/16

---
+++ src/Form/PropertyType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title',null,['label'=>'Titre'])
+            ->add('description',null,['label'=>'Description'])
+            ->add('surface')
+            ->add('rooms',null,['label'=>'Pièces'])
+            ->add('bedrooms',null,['label'=>'Chambres'])
+            ->add('floor',null,['label'=>'Étage'])
+            ->add('price',null,['label'=>'Prix'])
+            ->add('heat',ChoiceType::class,['label'=>'Type de chauffage', 'choices' => $this->getChoices()])
+            ->add('city',null,['label'=>'Ville'])
+            ->add('adress',null,['label'=>'Adresse'])
+            ->add('postal_code',null,['label'=>'CP'])
+            ->add('sold',null,['label'=>'Vendue ?'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+        ]);
+    }
+
+    private function getChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach ($choices as $k => $v){
+            $output[$v] = $k;
+        }
+        return $output;
+
+    }
+}
